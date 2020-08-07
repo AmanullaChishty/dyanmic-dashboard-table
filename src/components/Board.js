02a762302eb43d8040a7d81577e396bc53d1ca2a
Mohiuddin Amanulla Chishty <44812512+AmanullaChishty@users.noreply.github.com>
2020-08-07 15:00:59 +0530

    Update Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,12 +3,10 @@ import Title from './Title'
 import GridOnIcon from '@material-ui/icons/GridOn';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { DragableTable } from './DragableTable';
-import logo from './../images/logo-blk.png'
 export function Board(){
     return(
         <section className="main-board">
             <Title title='Web Design'/>
-            <img className="logo-styles" alt="logo" src={logo}/>
             <div className="table-sub-heading">
                 <GridOnIcon className="table-heading-text"/>
                 <p className="table-heading-text">Main Table</p>
